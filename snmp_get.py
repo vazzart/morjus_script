413db409ce2c25168e5a10ec37837262acebf666
Azat Khusainov <vazzart666@gmail.com>
2019-09-18 17:26:59 +0300

    remove arg

--- snmp_get.py
+++ snmp_get.py
@@ -66,11 +66,11 @@ def get_func(ip):
 
 
 if __name__ == "__main__":
-    if sys.argv[1]:
-        file_name = sys.argv[1]
-    else:
-        file_name = 'DES-1210-28_ME_B3.txt'
-#    file_name = 'DES-1210-28_ME_B3.txt'
+    # if sys.argv[1]:
+    #     file_name = sys.argv[1]
+    # else:
+    #     file_name = 'DES-1210-28_ME_B3.txt'
+    file_name = 'DES-1210-28_ME_B3.txt'
     OID = '.1.3.6.1.2.1.17.7.1.4.5.1.1.'
     community = 'holding08'
     snmp_port = 161
